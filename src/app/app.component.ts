bde6ad46539d72520a4f4258618fa39bd0c80e4e
Sophie H <marinsophie91@yahoo.fr>
2022-01-26 15:15:17 +0100

    tableau de snaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,64 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  deuxiemeSnap!: FaceSnap;
-  troisiemeSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = {
-      title: 'ourson',
-      description: "Ours en peluche dans l'herbe",
-      imageUrl:
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'dans un champs',
-    };
+    this.faceSnaps = [
+      {
+        title: 'ourson',
+        description: "Ours en peluche dans l'herbe",
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'dans un champs',
+      },
 
-    this.deuxiemeSnap = {
-      title: 'cheval qui se marre',
-      description: "une photo de cheval qui a l'air de rire",
-      imageUrl:
-        'https://tse1.mm.bing.net/th?id=OIP.UGe0hDgOlhZCZJhjMiiCjwHaEK&pid=Api&P=0&w=332&h=186',
-      createdDate: new Date(),
-      snaps: 0,
-    };
+      {
+        title: 'cheval qui se marre',
+        description: "une photo de cheval qui a l'air de rire",
+        imageUrl:
+          'https://tse1.mm.bing.net/th?id=OIP.UGe0hDgOlhZCZJhjMiiCjwHaEK&pid=Api&P=0&w=332&h=186',
+        createdDate: new Date(),
+        snaps: 0,
+      },
 
-    this.troisiemeSnap = {
-      title: 'Chien à bigoudis',
-      description: 'un chien avec des bigoudis sur la tête',
-      imageUrl:
-        'https://tse4.mm.bing.net/th?id=OIP.mUibgH7zai54OhVoxBwJIwHaE6&pid=Api&P=0&w=247&h=164',
-      createdDate: new Date(),
-      snaps: 0,
-    };
+      {
+        title: 'Chien à bigoudis',
+        description: 'un chien avec des bigoudis sur la tête',
+        imageUrl:
+          'https://tse4.mm.bing.net/th?id=OIP.mUibgH7zai54OhVoxBwJIwHaE6&pid=Api&P=0&w=247&h=164',
+        createdDate: new Date(),
+        snaps: 0,
+      },
+
+      {
+        title: 'Singe qui tire la langue',
+        description: 'un singe qui se fout de ta gueule!',
+        imageUrl:
+          'https://tse2.mm.bing.net/th?id=OIP.RGvbOLeN8QP51IZd2pUrfwHaFS&pid=Api&P=0&w=238&h=170',
+        createdDate: new Date(),
+        snaps: 0,
+      },
+
+      {
+        title: 'Ecurueil et mais',
+        description: 'un écurueil qui mange',
+        imageUrl:
+          'https://tse3.mm.bing.net/th?id=OIP.Wwg5-gXddv7O6NA7mnm8iwHaDw&pid=Api&P=0&w=311&h=158',
+        createdDate: new Date(),
+        snaps: 0,
+      },
+
+      {
+        title: 'Loutre surprise',
+        description: 'une loutre surprise',
+        imageUrl:
+          'https://tse1.mm.bing.net/th?id=OIP.s3FgNFeNeWdMYfuefUUvXgHaE7&pid=Api&P=0&w=231&h=153',
+        createdDate: new Date(),
+        snaps: 0,
+      },
+    ];
   }
 }
